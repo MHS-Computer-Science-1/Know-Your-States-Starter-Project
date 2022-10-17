1418c9ea7e9ca08fa1b8aa266b8f26c446b24080
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2022-10-17 09:47:00 -0400

    Add files via upload

---
+++ Know Your States/Know Your States/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+    Dim states(49), capitals(49) As String
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        'Display all the states and capitals
+        Dim i As Integer
+        For i = 0 To 49
+            ListBox1.Items.Add(states(i) & ", " & capitals(i))
+        Next
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Open file and load states and capitals into arrays
+        FileOpen(1, "states.txt", OpenMode.Input)
+
+        Dim i As Integer
+        For i = 0 To 49
+            Input(1, states(i))
+            Input(1, capitals(i))
+        Next
+    End Sub
+End Class
